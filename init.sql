1a4aef3991bc9d4c3902e4fffc3774b908d59245
Matt Baer <matt@flashstuf.com>
2015-10-14 11:24:26 -0400

    Add database schema

---
+++ init.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `houses`;
+CREATE TABLE `houses` (
+  `id` char(8) COLLATE utf8mb4_bin NOT NULL,
+  `html` text COLLATE utf8mb4_bin NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
