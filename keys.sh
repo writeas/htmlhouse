526891d9db60d3be98a17fb2c9568a9d7ab0e952
Matt Baer <matt@flashstuf.com>
2015-10-25 16:09:05 -0400

    Prevent running keys.sh with no params

--- keys.sh
+++ keys.sh
@@ -4,6 +4,11 @@
 # Utility script for generating a private/public key pair
 #
 
+if [[ -z "$1" ]]; then
+	echo "usage: ./keys.sh [key_basename]"
+	exit 1
+fi
+
 mkdir keys
 
 openssl genrsa -out keys/$1 2048
