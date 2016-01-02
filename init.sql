51086fd3e4ac2114df70a4cb6188b34c18bdaef4
Matt Baer <matt@flashstuf.com>
2016-01-02 14:56:47 -0500

    Add `publichouses` table schema

--- init.sql
+++ init.sql
@@ -6,3 +6,15 @@ CREATE TABLE `houses` (
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+DROP TABLE IF EXISTS `publichouses`;
+CREATE TABLE `publichouses` (
+  `house_id` char(8) NOT NULL,
+  `title` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `thumb_url` varchar(28) NOT NULL,
+  `added` datetime NOT NULL,
+  `updated` datetime NOT NULL,
+  `approved` tinyint(1) DEFAULT NULL,
+  `loves` int(4) NOT NULL,
+  PRIMARY KEY (`house_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
