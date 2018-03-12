7893f07a808f00a65c41777ac77bcab3708cbd20
Matt Baer <matt@write.as>
2018-03-12 11:28:20 -0400

    Automatically pass public filter when no blacklist configured

--- filter.go
+++ filter.go
@@ -1,6 +1,10 @@
 package htmlhouse
 
 func passesPublicFilter(app *app, html string) bool {
+	if app.cfg.BlacklistTerms == "" {
+		return true
+	}
+
 	spam := app.cfg.BlacklistReg.MatchString(html)
 	return !spam
 }
